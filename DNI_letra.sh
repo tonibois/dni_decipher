d6429860e4711c40b1b7317f2e10cc687cb7b2cb
Antonio Oliver Gelabert <toni.oliver87@gmail.com>
2020-10-05 08:00:22 +0200

    DNI_letra.sh

---
+++ DNI_letra.sh
@@ -0,0 +1,29 @@
+#!/BIN/BASH
+
+export div=23
+let gg=$@%$div
+
+if [ $gg == 0 ] ; then echo "Tu DNI es " $@"-T"; fi
+if [ $gg == 1 ] ; then echo "Tu DNI es " $@"-R"; fi
+if [ $gg == 2 ] ; then echo "Tu DNI es " $@"-W"; fi
+if [ $gg == 3 ] ; then echo "Tu DNI es " $@"-A"; fi
+if [ $gg == 4 ] ; then echo "Tu DNI es " $@"-G"; fi
+if [ $gg == 5 ] ; then echo "Tu DNI es " $@"-M"; fi
+if [ $gg == 6 ] ; then echo "Tu DNI es " $@"-Y"; fi
+if [ $gg == 7 ] ; then echo "Tu DNI es " $@"-F"; fi
+if [ $gg == 8 ] ; then echo "Tu DNI es " $@"-P"; fi
+if [ $gg == 9 ] ; then echo "Tu DNI es " $@"-D"; fi
+if [ $gg == 10 ] ; then echo "Tu DNI es " $@"-X"; fi
+if [ $gg == 11 ] ; then echo "Tu DNI es " $@"-B"; fi
+if [ $gg == 12 ] ; then echo "Tu DNI es " $@"-N"; fi
+if [ $gg == 13 ] ; then echo "Tu DNI es " $@"-J"; fi
+if [ $gg == 14 ] ; then echo "Tu DNI es " $@"-Z"; fi
+if [ $gg == 15 ] ; then echo "Tu DNI es " $@"-S"; fi
+if [ $gg == 16 ] ; then echo "Tu DNI es " $@"-Q"; fi
+if [ $gg == 17 ] ; then echo "Tu DNI es " $@"-V"; fi
+if [ $gg == 18 ] ; then echo "Tu DNI es " $@"-H"; fi
+if [ $gg == 19 ] ; then echo "Tu DNI es " $@"-L"; fi
+if [ $gg == 20 ] ; then echo "Tu DNI es " $@"-C"; fi
+if [ $gg == 21 ] ; then echo "Tu DNI es " $@"-K"; fi
+if [ $gg == 22 ] ; then echo "Tu DNI es " $@"-E"; fi
+
