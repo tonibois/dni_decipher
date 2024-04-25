b4658e6a9ba0c26d24131dfae42fa8309b39987d
Antonio Oliver Gelabert <toni_bois@hotmail.com>
2024-04-25 18:34:04 +0200

    Add files via upload

---
+++ DNI_letra.ps1
@@ -0,0 +1,32 @@
+﻿echo "Hola, escriu el teu DNI a continuació, o prem ENTER:"
+
+$dni= Read-Host
+
+Set-Variable -Name resto -Value ($dni % 23)
+
+echo "El resto de la divisió sencera entre el teu DNI i 23 és: $resto"
+
+
+if ($resto -eq 0) {echo "Per tant el teu DNI és: $dni-T"}
+if ($resto -eq 1) {echo "Per tant el teu DNI és: $dni-R"}
+if ($resto -eq 2) {echo "Per tant el teu DNI és: $dni-W"}
+if ($resto -eq 3) {echo "Per tant el teu DNI és: $dni-A"}
+if ($resto -eq 4) {echo "Per tant el teu DNI és: $dni-G"}
+if ($resto -eq 5) {echo "Per tant el teu DNI és: $dni-M"}
+if ($resto -eq 6) {echo "Per tant el teu DNI és: $dni-Y"}
+if ($resto -eq 7) {echo "Per tant el teu DNI és: $dni-F"}
+if ($resto -eq 8) {echo "Per tant el teu DNI és: $dni-P"}
+if ($resto -eq 9) {echo "Per tant el teu DNI és: $dni-D"}
+if ($resto -eq 10) {echo "Per tant el teu DNI és: $dni-X"}
+if ($resto -eq 11) {echo "Per tant el teu DNI és: $dni-B"}
+if ($resto -eq 12) {echo "Per tant el teu DNI és: $dni-N"}
+if ($resto -eq 13) {echo "Per tant el teu DNI és: $dni-J"}
+if ($resto -eq 14) {echo "Per tant el teu DNI és: $dni-Z"}
+if ($resto -eq 15) {echo "Per tant el teu DNI és: $dni-S"}
+if ($resto -eq 16) {echo "Per tant el teu DNI és: $dni-Q"}
+if ($resto -eq 17) {echo "Per tant el teu DNI és: $dni-V"}
+if ($resto -eq 18) {echo "Per tant el teu DNI és: $dni-H"}
+if ($resto -eq 19) {echo "Per tant el teu DNI és: $dni-L"}
+if ($resto -eq 20) {echo "Per tant el teu DNI és: $dni-C"}
+if ($resto -eq 21) {echo "Per tant el teu DNI és: $dni-K"}
+if ($resto -eq 22) {echo "Per tant el teu DNI és: $dni-E"}
