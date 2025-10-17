239a2d15c755e043cec49807eeab896d4fd51a13
Antonio Oliver Gelabert <toni_bois@hotmail.com>
2025-10-17 09:40:05 +0200

    Update DNI_sqlite.sh

--- DNI_sqlite.sh
+++ DNI_sqlite.sh
@@ -3,4 +3,4 @@
 echo "Writte your DNI below: "
 read number
 
-sqlite3 -line example.db "DROP TABLE IF EXISTS DNI; CREATE TABLE DNI(num int); INSERT INTO DNI(num) VALUES($number); SELECT num, SUBSTRING('TRWAGMYFPDXBNJZSQVHLCKE',num % 23+1,1) FROM DNI;"
+sqlite3 -line DNI.db "DROP TABLE IF EXISTS DNI; CREATE TABLE DNI(num int); INSERT INTO DNI(num) VALUES($number); SELECT num, SUBSTRING('TRWAGMYFPDXBNJZSQVHLCKE',num % 23+1,1) FROM DNI;"
